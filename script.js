63282d4a21b2046776acb35f939b90b600cf0da8
aaronh1986 <aaronharbourne@gmail.com>
2021-04-26 15:01:59 +0000

    Twenty two mins in

--- script.js
+++ script.js
@@ -3,16 +3,64 @@ const ctx = canvas.getContext('2d');
 
 let speed = 7;
 
+let tileCount = 20;
+let tileSize = canvas.width / tileCount -2;
+let headX = 10;
+let headY = 10;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
 // Game Loop
 function drawGame() {
     clearScreen();
+    changeSnakePosition();
+    drawSnake();
     setTimeout(drawGame, 1000/ speed);
-
 }
 
 function clearScreen() {
     ctx.fillStyle = "black";
-    
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake(){
+    ctx.fillStyle = "orange";
+    ctx.fillRect (headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition() {
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+document.body.addEventListener('keydown', keyDown);
+
+function keyDown(event) {
+    if (event.keyCode == 38) {
+        if(yVelocity == 1)
+            return;
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+    if (event.keyCode == 40) {
+        if(yVelocity == -1)
+            return;
+        yVelocity = 1;
+        xVelocity = 0;
+    }
+    if (event.keyCode == 37) {
+        if(xVelocity == 1)
+            return;
+        yVelocity = 0;
+        xVelocity = -1;
+    }
+    if (event.keyCode == 39) {
+        if(xVelocity == -1)
+            return;
+        yVelocity = 0;
+        xVelocity = 1;
+    }
 }
 
 drawGame();
